ee948e0ae4dbf372bdc193db55a7b0e15c3f7a71
shimbaa <shim9597@gmail.com>
2024-03-04 14:15:28 +0900

    feat: Pawn 생성
    Pawn 생성시 색상 정보 입력

---
+++ test/PawnTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import main.Pawn;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PawnTest {
+
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() {
+        Pawn whitePawn = new Pawn("white");
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+
+        Pawn blackPawn = new Pawn("black");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
+    }
+}
