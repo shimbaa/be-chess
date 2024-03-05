89de669c5bd03d75fe81fed064e570689f369f43
shimbaa <shim9597@gmail.com>
2024-03-05 14:29:53 +0900

    색상 지정되지 않은 폰 생성시 white를 기본값으로 설정

--- main/Pawn.java
+++ main/Pawn.java
@@ -2,10 +2,17 @@ package main;
 
 public class Pawn {
 
+    public static final String WHITE = "white";
+    public static final String BLACK = "black";
+
     private final String color;
 
     public Pawn(final String color) {
         this.color = color;
+    }
+
+    public Pawn() {
+        this.color = WHITE;
     }
 
     public String getColor() {
--- test/PawnTest.java
+++ test/PawnTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import main.Pawn;
 import org.junit.jupiter.api.DisplayName;
@@ -11,8 +12,8 @@ class PawnTest {
     @Test
     @DisplayName("폰 생성시 입력받은 색상 값")
     public void create() {
-        String black = "black";
-        String white = "white";
+        String black = Pawn.BLACK;
+        String white = Pawn.WHITE;
 
         verifyPawn(black);
         verifyPawn(white);
@@ -21,5 +22,12 @@ class PawnTest {
     private void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    @Test
+    @DisplayName("폰 생성시 지정된 색상값이 없다면 white가 기본값이어야 한다")
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals(Pawn.WHITE, pawn.getColor());
     }
 }
