e7a2d359d1c89c25e322913a709d8ae0a34a9613
shimbaa <shim9597@gmail.com>
2024-03-07 14:15:44 +0900

    각각 말에 알맞는 색상과 출력문자 값을 갖는 말 객체를 생성한다.

---
+++ chess/pieces/Piece.java
@@ -0,0 +1,66 @@
+package chess.pieces;
+
+public class Piece {
+    private final Name name;
+    private final Color color;
+
+    private Piece(Name name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(Name.PAWN, Color.WHITE);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(Name.PAWN, Color.BLACK);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(Name.KNIGHT, Color.WHITE);
+    }
+    public static Piece createBlackKnight() {
+        return new Piece(Name.KNIGHT, Color.BLACK);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(Name.ROOK, Color.WHITE);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Name.ROOK, Color.BLACK);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Name.BISHOP, Color.WHITE);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Name.BISHOP, Color.BLACK);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Name.QUEEN, Color.WHITE);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Name.QUEEN, Color.BLACK);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Name.KING, Color.WHITE);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Name.KING, Color.BLACK);
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public Representation getRepresentation() {
+        return this.name.findRepresentation(color);
+    }
+}
---
+++ test/chess/pieces/PieceTest.java
@@ -0,0 +1,39 @@
+package test.chess.pieces;
+
+import static org.assertj.core.api.Assertions.*;
+
+import chess.pieces.Color;
+import chess.pieces.Piece;
+import chess.pieces.Representation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PieceTest {
+
+    @Test
+    @DisplayName("해당 색상과 출력문자를 갖는 말을 생성해야한다")
+    void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Color.WHITE, Representation.PAWN_WHITE);
+        verifyPiece(Piece.createBlackPawn(), Color.BLACK, Representation.PAWN_BLACK);
+
+        verifyPiece(Piece.createWhiteKnight(), Color.WHITE, Representation.KNIGHT_WHITE);
+        verifyPiece(Piece.createBlackKnight(), Color.BLACK, Representation.KNIGHT_BLACK);
+
+        verifyPiece(Piece.createWhiteRook(), Color.WHITE, Representation.ROOK_WHITE);
+        verifyPiece(Piece.createBlackRook(), Color.BLACK, Representation.ROOK_BLACK);
+
+        verifyPiece(Piece.createWhiteBishop(), Color.WHITE, Representation.BISHOP_WHITE);
+        verifyPiece(Piece.createBlackBishop(), Color.BLACK, Representation.BISHOP_BLACK);
+
+        verifyPiece(Piece.createWhiteQueen(), Color.WHITE, Representation.QUEEN_WHITE);
+        verifyPiece(Piece.createBlackQueen(), Color.BLACK, Representation.QUEEN_BLACK);
+
+        verifyPiece(Piece.createWhiteKing(), Color.WHITE, Representation.KING_WHITE);
+        verifyPiece(Piece.createBlackKing(), Color.BLACK, Representation.KING_BLACK);
+    }
+
+    private void verifyPiece(final Piece piece, final Color color, Representation representation) {
+        assertThat(piece.getColor()).isEqualTo(color);
+        assertThat(piece.getRepresentation()).isEqualTo(representation);
+    }
+}
